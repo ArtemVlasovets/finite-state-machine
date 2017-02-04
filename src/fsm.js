874ed4d58f452823d16d780fe61db5e6ae93a64d
Artem Vlasovets <wlasowez.artem@mail.ru>
2017-02-05 00:26:34 +0300

    finite-state-machine/

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,57 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+      this.states = config.states;
+      this.activeState = this.initial = config.initial;
+      this.history = [];
+      this.undoHistory = [];
+      
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      return this.activeState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+       if (this.states[state]) {
+            this.history.push(this.activeState);
+            this.activeState = state;            
+        }
+        else {
+          throw new Error();
+          this.undoHistory = [];
+        }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      if (this.states[this.activeState].transitions[event]) {
+          this.changeState(this.states[this.activeState].transitions[event]);
+        }
+        else { 
+          throw new Error();
+        }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+      this.activeState = this.initial;
+      this.clearHistory();
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +61,71 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+      let array = [];
+      if (event == undefined) {
+        return Object.keys(this.states);
+      }
+      else {
+        if(this.states.normal.transitions.hasOwnProperty(event)) {
+          array.push('normal');
+        }
+        if(this.states.busy.transitions.hasOwnProperty(event)) {
+          array.push('busy');
+        }
+        if(this.states.hungry.transitions.hasOwnProperty(event)) {
+          array.push('hungry');
+        }
+        if(this.states.sleeping.transitions.hasOwnProperty(event)) {
+          array.push('sleeping');
+        }
+        return array;
+      }
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+      if (this.history.length > 0) {
+            let prevactiveState = this.history.pop();
+            this.undoHistory.push(this.activeState);
+            this.activeState = prevactiveState;
+            return true;
+        } 
+      else{
+        return false;
+      }
+        
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+      if (this.undoHistory.length > 0) {
+            let prevUndoState = this.undoHistory.pop();
+            this.history.push(this.activeState);
+            this.activeState = prevUndoState;
+            return true;
+        }else {
+            return false;
+
+      }
+        
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+      this.history = [];
+      this.undoHistory = [];
+    }
 }
 
 module.exports = FSM;
